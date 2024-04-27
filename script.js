57f84ed58c2c8afd298ab03b4895d0ab17013e64
mohammaduzer07 <134217616+mohammaduzer07@users.noreply.github.com>
2024-04-27 10:43:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,9 +6,14 @@ function insert_Row() {
 
 	const col1 = document.createElement("td");
 	const col2 = document.createElement("td");
+	
+	const text1 = document.createTextNode("New Cell1");
+    const text2 = document.createTextNode("New Cell2");
 
-	col1.textContent = "New Cell1";
-	col2.textContent = "New Cell2";
+	// col1.textContent = "New Cell1";
+	// col2.textContent = "New Cell2";
+	col1.appendChild(text1);
+    col2.appendChild(text2);
 	
 	newRow.appendChild(col1);
 	newRow.appendChild(col2);
