c31777e15bd5bab54549ab298fc3d7cbf22ff56d
mohammaduzer07 <134217616+mohammaduzer07@users.noreply.github.com>
2024-04-27 10:51:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,17 +7,12 @@ function insert_Row() {
 	const col1 = document.createElement("td");
 	const col2 = document.createElement("td");
 	
-	const text1 = document.createTextNode("New Cell1");
-    const text2 = document.createTextNode("New Cell2");
+	col1.textContent = "New Cell1";
+	col2.textContent = "New Cell2";
 
-	// col1.textContent = "New Cell1";
-	// col2.textContent = "New Cell2";
-	col1.appendChild(text1);
-    col2.appendChild(text2);
-	
 	newRow.appendChild(col1);
 	newRow.appendChild(col2);
 
-	table.insertBefore(newRow, table.firstChild);
-	
+	// table.insertBefore(newRow, table.firstChild);
+	table.tBodies[0].insertBefore(newRow, table.tBodies[0].firstChild);
 }
